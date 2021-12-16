33d05c2f2bd21d3f8d90724269e628bc88ce69d3
Furyton <1543159426@qq.com>
2021-12-16 21:11:41 +0800

    initial codes from branch `ensemble`

---
+++ scheduler/BaseSched.py
@@ -0,0 +1,40 @@
+"""
+
+tagging system -> prefix
+
+create logger_service
+create model
+create optim
+load model state
+load optim state
+create trainer
+train
+test
+
+pipeline?
+
+"""
+
+from abc import ABCMeta, abstractmethod
+
+# from torch import nn
+
+class _BaseSched(metaclass=ABCMeta):
+    def __init__(self) -> None:
+        pass
+
+    @abstractmethod
+    def _create_logger_service(self, prefix: str):
+        pass
+
+    @abstractmethod
+    def _fit(self):
+        pass
+
+    @abstractmethod
+    def _evaluate(self):
+        pass
+
+    @abstractmethod
+    def run(self):
+        pass
